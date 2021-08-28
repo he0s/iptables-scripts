49f3a7fdcbb6fae1dd59f591a96c4271f90ea1ab
Andrey Nikitin <heos@yandex.ru>
2021-08-28 16:07:31 +0400

    Add a new script to enable traffic forwarding

---
+++ 01_traffic_forwarding/iptables_forward.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+INP_IFACE="enp0s8"
+OUT_IFACE="enp0s3"
+
+echo "Implementing rules"
+iptables -A FORWARD -i "${INP_IFACE}" -j ACCEPT
+iptables -A FORWARD -o "${OUT_IFACE}" -j ACCEPT
+iptables -t nat -A POSTROUTING -o "${OUT_IFACE}" -j MASQUERADE
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
